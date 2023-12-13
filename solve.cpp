c1a576f42a679b437e97d66a8198df054a807fe7
Muhammah Salim <salimmia3745@gmail.com>
2023-12-13 15:10:16 +0600

    Is Subsequence(392) is solved with best complexity

--- solve.cpp
+++ solve.cpp
@@ -4,41 +4,15 @@ using namespace std;
 
 class Solution {
 public:
-	bool is_character(char c){
-		if((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')){
-			return true;
-		}
-		return false;
-	}
+    bool isSubsequence(string s, string t) {
+		int index_s = 0, index_t = 0;
 
-	char lower(char c){
-		if(c <= 'Z') c =  (c - 'A') + 'a';
-		return c;
-	}
-
-    bool isPalindrome(string s) {
-		int start_index = 0, last_index = s.size() - 1;
-
-		while(start_index < last_index){
-			if(!is_character(s[start_index])){
-				start_index++;
-				continue;
-			}
-			else if(!is_character(s[last_index])){
-				last_index--;
-				continue;
-			}
-
-			char start_char = lower(s[start_index]);
-			char last_char = lower(s[last_index]);
-
-			if(start_char != last_char) return false;
-
-			start_index++;
-			last_index;
+		while( index_t < t.size() && index_s < s.size()){
+			if(s[index_s] == t[index_t]) index_s++;
+			index_t++;
 		}
 
-		return true;
+		return index_s == s.size();
 	}
 };
 
@@ -46,11 +20,11 @@ void solve(int test_case){
 	string s, ss;
 	bool is_true;
 
-	cin >> s >> is_true;
+	cin >> s >> ss >> is_true;
 
 	Solution obj; 
 
-	if(obj.isPalindrome(s) == is_true){
+	if(obj.isSubsequence(s, ss) == is_true){
 		//cout << obj.convert(s, k) << " " << ss << endl;
 		cout << "Accepted"  << endl;
 	}
