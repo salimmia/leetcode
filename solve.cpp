703b5ec1307c1e7daa9fa82d969c14c0d4043451
Muhammah Salim <salimmia3745@gmail.com>
2023-12-13 15:45:44 +0600

    Two Sum II - Input Array Is Sorted(167) is solved using binary search)

--- solve.cpp
+++ solve.cpp
@@ -4,27 +4,56 @@ using namespace std;
 
 class Solution {
 public:
-    bool isSubsequence(string s, string t) {
-		int index_s = 0, index_t = 0;
+	int binary_search(vector<int>&numbers, int val){
+		int lo = 0, hi = numbers.size() - 1;
 
-		while( index_t < t.size() && index_s < s.size()){
-			if(s[index_s] == t[index_t]) index_s++;
-			index_t++;
+		while(lo < hi){
+			int mid = (lo + hi + 1) >> 1;
+			if(numbers[mid] <= val) lo =  mid;
+			else hi = mid - 1;
 		}
+		return lo;
+	}
 
-		return index_s == s.size();
+	vector<int>twoSum(vector<int>&numbers, int terget){
+		for(int index = 0; index < numbers.size() - 1; index++){
+			int need_index = binary_search(numbers, terget - numbers[index]); 
+			if(numbers[index] + numbers[need_index] == terget){
+				return {index + 1, need_index + 1};
+			}
+		}
+		return {0, 0};
 	}
 };
 
 void solve(int test_case){
-	string s, ss;
-	bool is_true;
+	int n, terget;
 
-	cin >> s >> ss >> is_true;
+	cin >> n >> terget;
 
-	Solution obj; 
+	vector<int>v;
 
-	if(obj.isSubsequence(s, ss) == is_true){
+	for(int i = 0; i < n; i++){
+		int x;
+		cin >> x;
+		v.push_back(x);
+	}
+
+	Solution obj;
+
+	vector<int>indexes = obj.twoSum(v, terget);
+	for(auto x: indexes) cout << x << " ";
+	cout << endl;
+
+	vector<int>ans;
+
+	for(int i = 0; i < 2; i++){
+		int x;
+		cin >> x;
+		ans.push_back(x);
+	}
+
+	if(ans == indexes){
 		//cout << obj.convert(s, k) << " " << ss << endl;
 		cout << "Accepted"  << endl;
 	}
